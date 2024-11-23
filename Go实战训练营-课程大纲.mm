e2a7d92b6be448670f207d20c5894973b0fe32d5
dabenxiong <747321301@qq.com>
2024-11-23 22:24:49 +0800

    feat[编写readme]: 编写readme
    [编写readme]

---
+++ Go实战训练营-课程大纲.mm
@@ -0,0 +1,176 @@
+
+<map>
+  <node ID="root" TEXT="Go实战训练营-课程大纲">
+    <node TEXT="1. 预习资源" ID="764d9c8318f9104d8466ec76a22a9e19" STYLE="bubble" POSITION="right">
+      <node TEXT="成长路线图" ID="1271131ccf8b34efe047cf706ae2043e" STYLE="fork">
+        <node TEXT="Learn to become a Go developer (roadmap.sh)" ID="70254263408915befaa6d7dc32a7e494" STYLE="fork"/>
+        <node TEXT="https://blog.csdn.net/a1369760658/article/details/137222776" ID="f17cd0aa02b2ef0b109424ec1474bf01" STYLE="fork"/>
+      </node>
+      <node TEXT="Go 基础知识" ID="170aac56154f4bdf8e6f9357fd738a4e" STYLE="fork">
+        <node TEXT="https://go.dev/tour/welcome/1" ID="d6f98cbebe9bb0348d7d3720e731fa99" STYLE="fork"/>
+      </node>
+      <node TEXT="Go 参考文档" ID="e28c998468a2e3bd9c646d2a3dc2fc2e" STYLE="fork">
+        <node TEXT="https://go.dev/doc/" ID="0d78bc512daa2dad7dfed096b26e1076" STYLE="fork"/>
+      </node>
+      <node TEXT="Go by Example - 带注释的示例程序" ID="4aac49ce436238e6220c48d634d6aa15" STYLE="fork">
+        <node TEXT="Go by Example" ID="feba778face575504d911cdccbffe00f" STYLE="fork"/>
+      </node>
+      <node TEXT="在 Go 中制作 RESTful JSON API" ID="db2d93599af8ead0dd60b08c6f94d404" STYLE="fork">
+        <node TEXT="Making a RESTful JSON API in Go - The New Stack" ID="2d84d26a8d855e329a6a2323e2f86b08" STYLE="fork"/>
+      </node>
+      <node TEXT="Go，云的编程语言" ID="022915cb807ffc3f53629d27e4a5e2aa" STYLE="fork">
+        <node TEXT="Go, the Programming Language of the Cloud - The New Stack" ID="f94d2e8f4cf103e336915aaff800431f" STYLE="fork"/>
+      </node>
+      <node TEXT="基础知识入门教程" ID="73864c0807584bd5fe7a1f821c2b96a0" STYLE="fork">
+        <node TEXT="8小时入门go语言开发|2023重制版_哔哩哔哩_bilibili" ID="31b19282d8a5278d51eb114bff9fcec3" STYLE="fork"/>
+      </node>
+      <node TEXT="基础知识重要概念context" ID="0326bfb9aaa788861d199b27d98e147e" STYLE="fork">
+        <node TEXT="20分钟搞懂go语言中的context_哔哩哔哩_bilibili" ID="a2b2eab568e17b4ed4f250c0f2e07b76" STYLE="fork"/>
+      </node>
+      <node TEXT="gRPC基础教程" ID="abd840e6c85153a7c8ba14b2af5a5ba6" STYLE="fork">
+        <node TEXT="【枫枫知道】手把手系列之gRPC基础教程 | Go语言_哔哩哔哩_bilibili" ID="559a907e074d0341d2d74fc885987720" STYLE="fork"/>
+      </node>
+      <node TEXT="go-zero零基础入门教程" ID="395c9e353a72a51845a1458e70006aaa" STYLE="fork">
+        <node TEXT="go-zero零基础入门教程|go微服务开发必学教程_哔哩哔哩_bilibili" ID="d08752b6051a612f20acbf93c4a0ff0f" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="2. 项目实战" ID="3f10b5a07257ddb32a4672081858d04d" STYLE="bubble" POSITION="right">
+      <node TEXT="Go课程导入（第1周: 9.3-9.6）" ID="831ad9ea8491d6bb6fbf8bd3cd570d8c" STYLE="fork">
+        <node TEXT="Go介绍（9.2）" ID="ab957e1d30927dbe0102d5ac5ec10bf7" STYLE="fork"/>
+        <node TEXT="环境配置（9.2）" ID="6c1b15fafe1fec2724366441486504ae" STYLE="fork"/>
+        <node TEXT="基础知识答疑群（9.2）" ID="62d88fb296a0e4687a5b5b50af65a92e" STYLE="fork"/>
+      </node>
+      <node TEXT="Go基础知识（自学预习）" ID="eafbe952bdbba8e573071713afe4a15a" STYLE="fork">
+        <node TEXT="Go命令、网络IO、磁盘IO操作" ID="cfbb9bb86297e78b5c30a29580b4ab85" STYLE="fork"/>
+        <node TEXT="变量、常量、类型、函数、包等 " ID="cc397fb165f813f0171dea108683fc6d" STYLE="fork"/>
+        <node TEXT="数组、切片" ID="cd03fc6e725f0bb680a5004a95a5b82b" STYLE="fork"/>
+        <node TEXT="指针、结构体、方法" ID="a46021d0c4a695ad4c4be4020ed0faa4" STYLE="fork"/>
+        <node TEXT="接口" ID="5618f8dcf13e1033760f4f43480b2d77" STYLE="fork"/>
+        <node TEXT="协程、管道、缓冲区、选择、互斥锁" ID="41fbba7b71200de328922d3f1fbfcef6" STYLE="fork"/>
+        <node TEXT="Defer、Error、Panic、Recover" ID="94d8a1ce23dc2847051709b01691c812" STYLE="fork"/>
+        <node TEXT="context基础" ID="d3d7f847e11c1d80cce21530ddf93355" STYLE="fork"/>
+        <node TEXT="grpc基础" ID="402e0e87f0eaf18981b092e5ec4017cb" STYLE="fork"/>
+      </node>
+      <node TEXT="go-zero微服务框架（第2/3周: 9.9-9.18）" ID="a5a25b89ac6e7cd07564217a08a68853" STYLE="fork">
+        <node TEXT="基础配置实例Go-zero介绍（9月9日）" ID="bc0b8aadf7d184fa934314630a6b58e3" STYLE="fork"/>
+        <node TEXT="goctl 代码生成工具(go-zero 生成工具讲解)（9月9日）" ID="c6bedccf3acb258ea2c3374c2be0e3da" STYLE="fork"/>
+        <node TEXT="api实例（9月10日）" ID="e0aa66e6589fb169413b9fd13d8ff7d5" STYLE="fork"/>
+        <node TEXT="gRPC实例（9月10日）" ID="c06f6170bd0aaa814d9552c763b889e6" STYLE="fork"/>
+        <node TEXT="mysql接入实例（9月10日）" ID="6d1533c1d3884c612b1008a1136dd8b3" STYLE="fork"/>
+        <node TEXT="Redis接入实例 Redis-Cluster（9月10日）" ID="4700311ea11812e186410cf4d00edab4" STYLE="fork"/>
+        <node TEXT="CRUD实例（用户）（9月10日）" ID="f533a3d61cac9982b7c1db5ba37a38b3" STYLE="fork"/>
+        <node TEXT="goctl 代码生成工具改造和模型修改（9月12日）" ID="cd06bab8843fe951dddff30a3e7a648f" STYLE="fork"/>
+        <node TEXT="gozc 代码生成工具(个人项目介绍)（9月12日）" ID="ce0815a969e9fa4dc116054ae6325630" STYLE="fork"/>
+        <node TEXT="gozc 代码生成工具改造和修改（9月13日）" ID="2df17f806d690913a60ef3c78d897b34" STYLE="fork"/>
+        <node TEXT="Tdengine  时序数据库（9月18日）" ID="4d0d93120c358ddabaa7f1b9a2ade9ee" STYLE="fork"/>
+        <node TEXT="redis-cluster 讲解(9月19日)" ID="fa6676335815900a0231c0b6de2867a1" STYLE="fork"/>
+        <node TEXT="asynmon  (理解miner和worker的概念)（9月19日）" ID="84a8d1562c65e3b79e53756d66617fd2" STYLE="fork">
+          <node TEXT="mq  缩小版的kakfa" ID="7091f4b61cc9df8bb590f62d58583de3" STYLE="fork"/>
+          <node TEXT="他是基于redis的 订阅和 发布" ID="0817d843ab2952f324b20bcd9bae0775" STYLE="fork"/>
+        </node>
+        <node TEXT="MQTT实例（9月19日）emqx  " ID="cc4ad1ecf3b834f9ccab0d70322467e0" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="3. 带教辅导" ID="4712dd41bd059c35b9f16b103c47aa00" STYLE="bubble" POSITION="right">
+      <node TEXT="物联网TPMT项目实战（第4-8周: 9.25-10.30）" ID="708434d1866410400aac13700cd36983" STYLE="fork">
+        <node TEXT="dockcer 使用中间件 环境搭建（9月25日）" ID="9d157b979d8109bcf17242d61743ca0b" STYLE="fork">
+          <node TEXT="项目介绍" ID="2c2b5d8696cc5f11bc8bccc68e30ad97" STYLE="fork"/>
+          <node TEXT="docker安装讲解" ID="6b1ab84260ec3eae1da7212073115518" STYLE="fork"/>
+          <node TEXT="docker-compose 讲解" ID="84847ae0a8164b1ec44e67860059a186" STYLE="fork"/>
+          <node TEXT="系统内使用中间件 和工具讲解" ID="707b5df1336be2e4e624366926de6904" STYLE="fork"/>
+          <node TEXT="docker安装环境讲解" ID="5fc3f471ff94f72a06c006a704051279" STYLE="fork"/>
+        </node>
+        <node TEXT="系统模块 （用户层）" ID="86f18d97dc5593d52a31547bac6c37c2" STYLE="fork">
+          <node TEXT="用户CRUD（9月26日）" ID="643cbe4b16b786e44646354c5e22ed9a" STYLE="fork"/>
+          <node TEXT="角色CRUD（10月10日）" ID="41e9dc5c6b81c63e799ce401b4e94b58" STYLE="fork"/>
+          <node TEXT="菜单CRUD（10月10日）" ID="fc599e85543cd731705b34fcbedf8db5" STYLE="fork"/>
+          <node TEXT="接口CRUD（10月10日）" ID="aed8e78884e8ae9899c68200c442cdf0" STYLE="fork"/>
+          <node TEXT="第三方用户CRUD（10月12日）" ID="0244ad8279e542a10e3c41bb103f7dbc" STYLE="fork"/>
+          <node TEXT="用户登录（9月26日）" ID="e30ce8a6972041b25e165301d82f8c47" STYLE="fork"/>
+        </node>
+        <node TEXT="信息库模块" ID="c968d954aaee4d0694fba6d2085c0d75" STYLE="fork">
+          <node TEXT="字典类型CRUD（10月12日）" ID="136a0e0e34a7e24106d81effeb940a9e" STYLE="fork"/>
+          <node TEXT="字典内容CRUD（10月12日）" ID="3e298c3ace9cabeaacbb3298e6468222" STYLE="fork"/>
+          <node TEXT="资产CRUD（10月17日）" ID="9e06813debe33a648e9e76df8e2b1328" STYLE="fork"/>
+          <node TEXT="串口CRUD（10月17日）" ID="634404ba3227815d2af4371d537799fc" STYLE="fork"/>
+          <node TEXT="网关CRUD（10月17日）" ID="40b4e449926493b762cb126559aa6331" STYLE="fork"/>
+          <node TEXT="监测点CRUD（10月17日）" ID="c8da65107b9a44bacef6bab676e7fae8" STYLE="fork"/>
+          <node TEXT="定时任务（10月24日）待定" ID="7c46a20d31863f6aafe37708e083592f" STYLE="fork"/>
+          <node TEXT="重试任务（10月24日）待定" ID="4f52ab2a3471f3c94b54d7b87c77375c" STYLE="fork"/>
+        </node>
+        <node TEXT="在线监测模块" ID="cff7bf80c22d423d9112cefb57eda54c" STYLE="fork">
+          <node TEXT="监测点获取实时数据（10月24日）" ID="d2721ae6a2c1111434de7a598a7df4b8" STYLE="fork"/>
+          <node TEXT="监测点获取历史数据（10月24日）" ID="c2b5085b29ce189338c68f96ba7da482" STYLE="fork"/>
+        </node>
+        <node TEXT="设备通讯存储tpmt-com" ID="7b89504661bda14f9ad6c6653949fc18" STYLE="fork">
+          <node TEXT="ants协程池介绍（10月23日）" ID="9eeb1f1979bb4129bf5e74005c6e68c5" STYLE="fork"/>
+          <node TEXT="模拟串口接收数据（10月23日）" ID="de346bd3f6122bcef8d996cf1075cce8" STYLE="fork"/>
+          <node TEXT="串口取数据存储（10月23日）" ID="5fe8bd44ab4174f05f8bc00d797e6b89" STYLE="fork"/>
+          <node TEXT="串口定时循环数据上发（10月23日）" ID="9fee0318f8b406259d039bd08fc80437" STYLE="fork"/>
+          <node TEXT="串口websocket监控设备通讯信息（10月24日）" ID="1d0587033b767638618638989a0bb57d" STYLE="fork"/>
+        </node>
+        <node TEXT="miner-worker 程序" ID="f3fe622503eed1e53eff358269ccc685" STYLE="fork">
+          <node TEXT="任务一（10月23日）" ID="239080b79255451507a66378a1e1ca7f" STYLE="fork">
+            <node TEXT="实时数据任务 " ID="51d52805474947e7abf07bbb11d8589d" STYLE="fork"/>
+            <node TEXT="1.接收mq实时数据任务 " ID="5b9f44ff73ef5aa33d19a1d824059774" STYLE="fork"/>
+            <node TEXT="2.存储到redis用于实时数据" ID="5ab1d375bfaf7578d35ea18869ee797c" STYLE="fork"/>
+            <node TEXT="3.存储到时序数据库用于历史数据" ID="e5eba368a0315ebc8d9c32d1d55b99a7" STYLE="fork"/>
+            <node TEXT="4.发布异步告警任务判断是否告警" ID="84ce8a843bfff6d95b6656e404f471fc" STYLE="fork"/>
+          </node>
+          <node TEXT="任务二（10月23日）" ID="a1c6757b673fad118e2db3cbc88cb75d" STYLE="fork">
+            <node TEXT="告警任务:" ID="ec0356743f2ac315a35f9083322a017e" STYLE="fork"/>
+            <node TEXT="1.判断是否告警" ID="f7f760956886d192c52504707cf7fe10" STYLE="fork"/>
+            <node TEXT="2.判断数据是否在告警阻塞" ID="d71a6c933615a4c89a41d040837d2a49" STYLE="fork"/>
+            <node TEXT="3.写入时序数据告警信息" ID="179bc9dfcb55b763273b632f98656eef" STYLE="fork"/>
+          </node>
+          <node TEXT="任务三（10月23日）" ID="c66b4c2aae81e17dc1996341243ace08" STYLE="fork">
+            <node TEXT="网关离线告警:" ID="be247f35f17a78981466e29bd5c65b38" STYLE="fork"/>
+            <node TEXT="1.根据连接信息判断网关是否离线" ID="0d10966b533d9d695624967730e07ed0" STYLE="fork"/>
+            <node TEXT="2.判断网关是否在告警阻塞" ID="3ba6bcbc4bf68bd1f194054588e3fbd7" STYLE="fork"/>
+            <node TEXT="3.写入时序数据库网关离线通知" ID="77ba52bcf229654410996866a68fda7d" STYLE="fork"/>
+          </node>
+          <node TEXT="任务四（10月24日） 待定" ID="a220fd61322e5a03792f2afe3be2d496" STYLE="fork">
+            <node TEXT="定时任务：" ID="72994b4a7e6bd3580750badd0109b1b9" STYLE="fork"/>
+            <node TEXT="1.查询所有定时任务信息" ID="97c35773ef33a20d8a4f540764ba07d2" STYLE="fork"/>
+            <node TEXT="2.根据不同的定时任务执行不同的内容和策略北向传输调用北向传输（mqttsend）rpc" ID="24b15e20b7d37e19708ed004faa6cb90" STYLE="fork"/>
+          </node>
+        </node>
+        <node TEXT="weboscket服务" ID="c430ba988ee39de4a8ccc50ccfaaccb9" STYLE="fork">
+          <node TEXT="1、简单使用搭建（10月20日）" ID="80ae0519d5e10e6bd80bb10195717691" STYLE="fork"/>
+          <node TEXT="2、查询实时数据（10月24日）" ID="027dcfc6db3aa570aec062af32523043" STYLE="fork"/>
+          <node TEXT="3、发送告警实时通知（10月24日）" ID="820bcf5d7ce5f5b36f986b0f166c763f" STYLE="fork"/>
+          <node TEXT="4、用户聊天模拟，设备下发指令（10月31日） " ID="94139af78f4c384ebc05b76fdbcd11ad" STYLE="fork"/>
+        </node>
+        <node TEXT="日志服务 archive" ID="c9266562aec1955185d8fe46a3d3e892" STYLE="fork">
+          <node TEXT="日志设计和存储Rpc编写（10月15日）" ID="ec26e83681611677a14e53b32bf03e36" STYLE="fork"/>
+          <node TEXT="查询操作日志（10月15日）" ID="349e54236f027f316ab1ce7686f21560" STYLE="fork"/>
+          <node TEXT="查询定时任务/重试任务日志（10月15日）" ID="004a6f9d897de2f2ef23c2f6425b3acf" STYLE="fork"/>
+          <node TEXT="查询告警和通知信息（10月15日）" ID="23c56b7d3f854f8d6915e025783090da" STYLE="fork"/>
+          <node TEXT="更新告警状态（10月15日）" ID="284d2954fe2ce00edc5765c6f1f7a109" STYLE="fork"/>
+          <node TEXT="用户聊天记录（10月15日）" ID="031b52e345fdc219e7a465cdd8fc1672" STYLE="fork"/>
+        </node>
+        <node TEXT="鉴权微服务 authentication " ID="83dcca5ddbee041b37fefb8ec36baa6a" STYLE="fork">
+          <node TEXT="提供用户身份认证信息（11月06日）" ID="9e5cbe6eba621401a10e9637674388a5" STYLE="fork"/>
+        </node>
+        <node TEXT="发送服务mqttsend" ID="0f1a07c95def37e265811146d0713bb0" STYLE="fork">
+          <node TEXT="使用websocket 发送MQTT数据（10月24日） 待定" ID="97a026331f5b77b3b70f7d96cf1e7daa" STYLE="fork"/>
+        </node>
+        <node TEXT="项目编译/部署（10月31日） " ID="46d67a09a3f73d8fba3fc432a559b9ea" STYLE="fork">
+          <node TEXT="makefile介绍和cmake简单介绍" ID="6689c65fc1116064ccc1e9e37b82a9fc" STYLE="fork"/>
+          <node TEXT="跨平台编译" ID="b1ef89a06a5ac48864bcdbe9338fa380" STYLE="fork"/>
+          <node TEXT="docker部署  " ID="83bc22f257080a65b2e7d073cd82c705" STYLE="fork"/>
+          <node TEXT="项目运维排查" ID="7c450a5c6cbb4961e43d07ac8e76c89c" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="4. 共同成长" ID="eff813ebf849c11bb339aaac9d5940b4" STYLE="bubble" POSITION="right">
+      <node TEXT="Go设计模式（第9-12周：11.4日起）" ID="888fe8d148662d1f8f7097d8849c3b4b" STYLE="fork"/>
+      <node TEXT="Go与人工智能" ID="001c09ab839f680134b1a442193f9b6e" STYLE="fork">
+        <node TEXT="Gorgonia" ID="038676e902cd1d9c6b711717b0c4ca14" STYLE="fork"/>
+        <node TEXT="GoLearn" ID="a1a6b1166f4514e9210bc928ff8eb5fe" STYLE="fork"/>
+        <node TEXT="Go–onnx" ID="f10054d9448f5f349cf7f0058363185a" STYLE="fork"/>
+      </node>
+      <node TEXT="结营回顾" ID="1ed40f787af006027d347ce32ab1a860" STYLE="fork"/>
+    </node>
+  </node>
+</map>
